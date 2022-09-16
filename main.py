88e82855bade19144193d241bfd7e4a6ac179426
Erutuf-Roy <erutuf.blueshiner@gmail.com>
2022-09-16 12:56:25 +0800

    Update features: tunable gamma

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import scipy
+import pandas as pd
 
 import torch
 import torchaudio
@@ -32,6 +33,7 @@ topic = st.selectbox("Select a PP-PCS version",
         (
             '(1) Original PP-PCS', 
             '(2) PP-PCS400',
+            '(3) Original PP-PCS (with tunable parameters)'
         )
     )
 
@@ -133,6 +135,89 @@ elif topic == '(2) PP-PCS400':
     PCS[130:160] = 1.238596491
     PCS[160:190] = 1.161403509
     PCS[190:202] = 1.077192982
+
+elif topic == '(3) Original PP-PCS (with tunable parameters)':
+    st.header("PP-PCS tunable parameters")
+
+    n_fft = 512
+    hop_length = 256
+
+    PCS = torch.ones(257)      # Perceptual Contrast Stretching
+    PCS[0:3] = 1
+    PCS[3:6] = 1.070175439
+    PCS[6:9] = 1.182456140
+    PCS[9:12] = 1.287719298
+    PCS[12:138] = 1.4       # Pre Set
+    PCS[138:166] = 1.322807018
+    PCS[166:200] = 1.238596491
+    PCS[200:241] = 1.161403509
+    PCS[241:256] = 1.077192982
+
+    BIF = torch.ones(9)
+    BIF[0] = 0.0000
+    BIF[1] = 0.010
+    BIF[2] = 0.026
+    BIF[3] = 0.041
+    BIF[4] = 0.057
+    BIF[5] = 0.046
+    BIF[6] = 0.034
+    BIF[7] = 0.023
+    BIF[8] = 0.011
+
+    colG1, colG2 = st.columns(2)
+    with colG1:
+        g_min = st.slider(
+                'Minimun of gamma:',
+                0.0, 5.0, value=1.0, step=0.01)
+    with colG2:
+        g_max = st.slider(
+                'Maximum of gamma:',
+                g_min, 5.0, value=1.4, step=0.01)
+    if g_min >= g_max:
+        st.error('Error: Minimum should smaller than maximum', icon="🚨")
+    else:
+        PCS = torch.ones(257)      # Perceptual Contrast Stretching
+        PCS[0:3]        = (g_max - g_min) / 0.057 * BIF[0] + g_min
+        PCS[3:6]        = (g_max - g_min) / 0.057 * BIF[1] + g_min
+        PCS[6:9]        = (g_max - g_min) / 0.057 * BIF[2] + g_min
+        PCS[9:12]       = (g_max - g_min) / 0.057 * BIF[3] + g_min
+        PCS[12:138]     = (g_max - g_min) / 0.057 * BIF[4] + g_min
+        PCS[138:166]    = (g_max - g_min) / 0.057 * BIF[5] + g_min
+        PCS[166:200]    = (g_max - g_min) / 0.057 * BIF[6] + g_min
+        PCS[200:241]    = (g_max - g_min) / 0.057 * BIF[7] + g_min
+        PCS[241:256]    = (g_max - g_min) / 0.057 * BIF[8] + g_min
+
+    f_x = torch.ones(8000)
+    f_x[0:100] = PCS[0]
+    f_x[100:200] = PCS[3]
+    f_x[200:300] = PCS[6]
+    f_x[300:400] = PCS[9]
+    f_x[400:4400] = PCS[12]
+    f_x[4400:5300] = PCS[138]
+    f_x[5300:6400] = PCS[166]
+    f_x[6400:7700] = PCS[200]
+    f_x[7700:8000] = PCS[241]
+
+    """
+    f_o = torch.ones(8000)
+    f_o[0:100] = 1
+    f_o[100:200] = 1.070175439
+    f_o[200:300] = 1.182456140
+    f_o[300:400] = 1.287719298
+    f_o[400:4400] = 1.4
+    f_o[4400:5300] = 1.322807018
+    f_o[5300:6400] = 1.238596491
+    f_o[6400:7700] = 1.161403509
+    f_o[7700:8000] = 1.077192982
+    """
+
+
+    chart_data = pd.DataFrame(
+            f_x.numpy(),
+            columns=['Gamma-PCS'])
+    st.area_chart(chart_data)
+    
+
 
 # ------- Upload Clean Wav -------
 st.subheader("You can upload the audio manually here or use the sample audio.")
